c0d5defa1cc0174c541653302bcd2e80caa4e0c5
おりょう <oryo.developer@gmail.com>
2024-06-11 20:18:12 +0900

    Appを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/app.dart';
 import 'package:flutter_chat_app/firebase_options.dart';
 
 Future<void> main() async {
@@ -7,20 +8,5 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(const App());
 }
